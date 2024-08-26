ef255aa7718b5a5ce2ed91c01b6657fa67558d1d
apexape84 <apexape84@gmail.com>
2024-08-26 17:40:37 +0900

    prac1,2 완료

---
+++ Application/src/main/java/com/ogiraffers/practice/hi0826prac02.java
@@ -0,0 +1,11 @@
+package com.ogiraffers.practice;
+
+public class hi0826prac02 {
+    public static void main(String[] args) {
+        double width = 12.5;
+        double height = 36.4;
+        System.out.println("면적 : "+(width*height));
+        System.out.println("둘레 : "+2*(width+height));
+
+    }
+}
