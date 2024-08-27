c1cb010efb2d0eac5322c273c3d7945b620e99fd
apexape84 <apexape84@gmail.com>
2024-08-27 12:53:17 +0900

    예제3

---
+++ Application/src/main/java/com/ogiraffers/practice/hi0827prac03.java
@@ -0,0 +1,16 @@
+package com.ogiraffers.practice;
+
+public class hi0827prac03 {
+    public static void main(String[] args) {
+        double korean = 80.5;
+        double math = 50.6;
+        double english = 70.8;
+
+        double total = korean + math + english;
+        double average = total / 3;
+
+        System.out.println("총점 : "+(int)total);
+        System.out.println("평균 : "+(int)average);
+
+    }
+}
---
+++ Application/src/main/java/com/ogiraffers/section05/typecasting/application3.java
@@ -0,0 +1,7 @@
+package com.ogiraffers.section05.typecasting;
+// title
+public class application3 {
+    public static void main(String[] args) {
+
+    }
+}
