886434ac567d30022110866a7a9f6e7ea4bbc3e7
apexape84 <apexape84@gmail.com>
2024-08-27 17:42:25 +0900

    정현prac 업데이트

---
+++ chap02-operator/src/main/java/com/ogiraffers/practice/jh0827prac01.java
@@ -0,0 +1,10 @@
+package com.ogiraffers.practice;
+
+public class jh0827prac01 {
+    public static void main(String[] args) {
+        double weight = 55.5;
+        int height = 160;
+
+        System.out.println("정현님의 BMI는 "+55.5/(double)(height*height)*10000);
+    }
+}
---
+++ chap02-operator/src/main/java/com/ogiraffers/section01/arithmetic/application2.java
@@ -0,0 +1,10 @@
+package com.ogiraffers.section01.arithmetic;
+
+public class application2 {
+    public static void main(String[] args) {
+        String name1 = "이승현";
+        String name2 = "이승현";
+
+        System.out.println("name1 과 name2 가 같은가? " + (name1==name2));
+    }
+}
